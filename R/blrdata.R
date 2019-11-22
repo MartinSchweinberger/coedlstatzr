797569537d174030c514b7078acbaa8d55c1063a
Martin Schweinberger <martin.schweinberger.hh@gmail.com>
2019-11-22 15:13:38 +1000

    commit 20191122_1513

---
+++ R/blrdata.R
@@ -0,0 +1,13 @@
+#' Coding of 5,000 Speech-Units from of spoken colloquial New Zealand English
+#'
+#' A dataset containing the coding of 5,000 speech-units of New Zealand English for instances of speech-unit final EH, a speaker id as well as the age, gender, and ethnicity of the speaker.
+#'
+#' @format A data frame with 5,000 rows and 5 variables:
+#' \describe{
+#'   \item{ID}{speaker identification}
+#'   \item{Gender}{gender of speaker (Men versus Women)}
+#'   \item{Age}{age of speaker (Young versus Old)}
+#'   \item{Ethnicity}{ethnicity of speaker (Maori versus Pakeha)}
+#'   ...
+#' }
+"blrdata"
---
+++ R/cfadata.R
@@ -0,0 +1,14 @@
+#' Coding of 250 cases of a fictional data set
+#'
+#' A dataset containing the 250 cases representing speakers with certain sociodemographic features and their use of an unnamed linguistic feature.
+#'
+#' @format A data frame with 5,000 rows and 5 variables:
+#' \describe{
+#'   \item{Variety}{vareity of English (American versus British)}
+#'   \item{Gender}{gender of speaker (Men versus Women)}
+#'   \item{Age}{age of speaker (Young versus Old)}
+#'   \item{Class}{socioeconomic class of speaker (Middle versus Working)}
+#'   \item{Frequency}{freqeuncy counts of an unnamed linguistic feature}
+#'   ...
+#' }
+"cfadata"
