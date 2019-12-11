d2dfe7f2a188b265688bcbe265a3908cefcc96d3
Adam Sparks <adamhsparks@gmail.com>
2019-12-11 11:58:27 +1000

    Fixes documentation for data and other warnings when checking

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,11 +1,15 @@
 #' @title Multiple ggplots in one Graphics Window
 #'
 #' @description This function allows the display of multiple ggplots in a single graphics window.
-#' @param x A vector of numbers representing the panel which a plot will occupy.
+#' @param ... Further options passed to \pkg{ggplot} and \pkg{grid}
+#' @param plotlist THIS NEEDS TO BE DOCUMENTED
+#' @param file THIS NEEDS TO BE DOCUMENTED
+#' @param cols THIS NEEDS TO BE DOCUMENTED
+#' @param layout THIS NEEDS TO BE DOCUMENTED
 #' @export
 #' @keywords ggplot2, multiple plots, function
 #' @return NULL
-#' @import ggplot2, grid
+#' @import ggplot2 grid
 #' @examples \dontrun{
 #' #p1 <- ggplot(data, aes(var1, var2)) +
 #' #geom_point()
@@ -14,8 +18,8 @@
 #' #multiplot(p1, p2, cols = 1)
 #' }
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  require(ggplot2)
-  require(grid)
+  requireNamespace(ggplot2)
+  requireNamespace(grid)
   plots <- c(list(...), plotlist)
   numPlots = length(plots)
   if (is.null(layout)) {
